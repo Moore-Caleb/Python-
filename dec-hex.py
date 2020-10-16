272fd8fa3d2323c7992b564b0ce9d10a80afc022
Warghost13 <47615426+Warghost13@users.noreply.github.com>
2020-10-16 09:07:37 -0700

    Add files via upload

---
+++ dec-hex.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# Base 10 to base 16 Dex-hex.py
+print ("DECIMAL", 1,"= HEXADECIMAL", hex(1))
+print ("DECIMAL", 2,"= HEXADECIMAL", hex(2))
+print ("DECIMAL", 3,"= HEXADECIMAL", hex(3))
+print ("DECIMAL", 4,"= HEXADECIMAL", hex(4))
+print ("DECIMAL", 5,"= HEXADECIMAL", hex(5))
+print ("DECIMAL", 6,"= HEXADECIMAL", hex(6))
+print ("DECIMAL", 7,"= HEXADECIMAL", hex(7))
+print ("DECIMAL", 8,"= HEXADECIMAL", hex(8))
+print ("DECIMAL", 9,"= HEXADECIMAL", hex(9))
+print ("DECIMAL", 10,"= HEXADECIMAL", hex(10))
+print ("DECIMAL", 11,"= HEXADECIMAL", hex(11))
+print ("DECIMAL", 12,"= HEXADECIMAL", hex(12))
+print ("DECIMAL", 13,"= HEXADECIMAL", hex(13))
+print ("DECIMAL", 14,"= HEXADECIMAL", hex(14))
+print ("DECIMAL", 15,"= HEXADECIMAL", hex(15))
+print ("DECIMAL", 16,"= HEXADECIMAL", hex(16))
+
+'''
+10^3	10^2	10^1	10^0
+1,000	100		10		1
+
+16^7	16^6	16^5	16^4	16^3	16^2	16	16^0
+268.435.456	16.777.216	1.048.576	65.536	4096	256	16	1
+'''
