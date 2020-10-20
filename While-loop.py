cedc20a3fc29a2beae2267ea6e749358437f83bb
Warghost13 <47615426+Warghost13@users.noreply.github.com>
2020-10-20 09:25:39 -0700

    Add files via upload

---
+++ While-loop.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python 3
+# while-loop.py
+count = 0
+while(count < 256 ):
+	print(str(count)+"  ",end="" )
+	count = count + 1
+	if(count % 20 == 0):
+		print(" * ")
+
+'''
+count uncrement 
+encapsulated block - scope
+type cast
+concatenation 
+% modulus modulo
+'''
